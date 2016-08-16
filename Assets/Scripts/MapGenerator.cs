b72abe38c15a866b911a1c0f9be5bfd9578985c9
Jens Langerak <J.Langerak@student.tudelft.nl>
2016-08-16 18:45:03 +0200

    noise map generator

---
+++ Assets/Scripts/MapGenerator.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MapGenerator : MonoBehaviour {
+	public long seed;
+	public int mapWith;
+	public int mapHeight;
+	public float scale;
+	NoiseMapGenerator noiseMapGenerator;
+	public int coordX;
+	public int coordY;
+	public bool autoUpdate;
+
+	public void generateMap() {
+		if (seed == 0) {
+			seed = System.DateTime.Now.Ticks;
+		}
+		System.Random randomGenerator = new System.Random((int) seed);
+
+		float offsetX = randomGenerator.Next (-100000, 100000);
+		float offsetY = randomGenerator.Next (-100000, 100000);
+		Vector2 seedOffset = new Vector2 (offsetX, offsetY);
+		
+		noiseMapGenerator = new NoiseMapGenerator (scale, seedOffset);
+		float[,] map = noiseMapGenerator.getNoiseMap (mapWith, mapHeight, coordX, coordY);
+
+		MapDisplay display = FindObjectOfType<MapDisplay> ();
+		display.Draw (map);
+
+	}
+}
---
+++ Assets/Scripts/NoiseMapGenerator.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NoiseMapGenerator {
+	protected float scale;
+	protected Vector2 seedOffset;
+
+
+	public NoiseMapGenerator(float scale, Vector2 seedOffset) {
+		this.seedOffset = seedOffset;
+		this.scale = scale;
+	}
+
+	public float[,] getNoiseMap(int width, int height, int offsetX, int offsetY) {
+		float[,] noiseMap = new float[width, height];
+
+		float centerX = ((float)width) / 2.0f;
+		float centerY = ((float)height) / 2.0f;
+
+		for (int x = 0; x < width; x++) {
+			for (int y = 0; y < height; y++) {
+				float coordX = x + offsetX;
+				float coordY = y + offsetY;
+				float hexOffset = ((coordX % 2) == 0.0f) ? 0 : 0.5f;
+				float sampleX = ((coordX - centerX) + seedOffset.x) * scale;
+				float sampleY = ((coordY - centerY) + seedOffset.y + hexOffset) * scale;
+
+				noiseMap [x, y] = Mathf.PerlinNoise (sampleX, sampleY);
+			}
+
+		}
+
+		return noiseMap;
+	}
+
+}
