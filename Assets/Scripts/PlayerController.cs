f1ea3642ed52a047cb2966b14f41fa4deff1e00b
Jens Langerak <J.Langerak@student.tudelft.nl>
2016-08-17 12:47:44 +0200

    fly over map

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+	public float speed;
+	public float rotateSpeed;
+	public Transform camera;
+
+	void Update()
+	{
+		float moveHorizontal = Input.GetAxis("Horizontal");
+		float moveVertical = Input.GetAxis("Vertical");
+		float rotate = Input.GetAxis("Rotate");
+		float moveY = Input.GetAxis("UpDown");
+		float rotateX = Input.GetAxis("RotateX");
+
+		Vector3 movement = new Vector3(moveHorizontal, moveY, moveVertical);
+		
+		transform.Translate(movement * Time.deltaTime * speed);
+
+		transform.Rotate(Vector3.up * rotate * rotateSpeed * Time.deltaTime);
+
+		camera.Rotate(Vector3.right * rotateX * rotateSpeed * Time.deltaTime);
+	}
+}
